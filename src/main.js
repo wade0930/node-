3c2c9d46624716ced8390c952489cfcc71efb8a8
DESKTOP-4DPQSAF\yth10 <yth102345@gmail.com>
2020-08-07 18:03:58 +0800

    註冊和登入完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import axios from "./http"
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+Vue.use(ElementUI)
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,38 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Index from "@/views/Index.vue"
+import Register from "@/views/Register.vue"
+import NotFound from "@/views/404.vue"
+import Login from "@/views/Login.vue"
 
 Vue.use(VueRouter)
 
   const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/index',
+    name: 'index',
+    component: Index
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/',
+    redirect: '/index'
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '*',
+    name: '/404',
+    component: NotFound
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+
 ]
 
 const router = new VueRouter({
